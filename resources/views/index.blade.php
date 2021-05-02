03bba5d33a92871c56d225a8a455d6c0920ed3d5
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-05-02 13:02:19 +0700

    init project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot name="title">
+        Obat
+    </x-slot>
+
+    <x-slot name="subtitle">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt et vel unde, quae culpa alias repellendus.
+    </x-slot>
+
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">Obat</h4>
+        </div>
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi? Ullam quaerat
+            similique iusto
+            temporibus, vero aliquam praesentium, odit deserunt eaque nihil saepe hic deleniti? Placeat
+            delectus
+            quibusdam ratione ullam!
+        </div>
+    </div>
+</x-layout>
