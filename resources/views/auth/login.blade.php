dee578de06746267c7a405a83f987b734bd13273
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-05-04 11:31:13 +0700

    ubah tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,83 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Mazer Admin Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
+</head>
+
+<body>
+    <div id="auth">
+
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="index.html">
+                            <h1>Paragon</h1>
+                        </a>
+                    </div>
+                    <h1 class="auth-title">Log in.</h1>
+                    {{-- <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p> --}}
+
+                    <!-- Session Status -->
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" placeholder="Username"
+                                name="username" value="{{ old('username') }}" required>
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password"
+                                name="password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" type="submit">Log in</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+                        <p class="text-gray-600">
+                            Don't have an account?
+                            <a href="{{ url('register') }}" class="font-bold">
+                                Sign up
+                            </a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
+
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 <span class="text-2xl">Paragon Login</span>
             </a>
         </x-slot>
@@ -14,44 +89,45 @@
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
-            @csrf
+@csrf
 
-            <!-- Username -->
-            <div>
-                <x-label for="username" :value="__('Username')" />
+<!-- Username -->
+<div>
+    <x-label for="username" :value="__('Username')" />
 
-                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
-            </div>
+    <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required
+        autofocus />
+</div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+<!-- Password -->
+<div class="mt-4">
+    <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+        autocomplete="current-password" />
+</div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+<!-- Remember Me -->
+<div class="block mt-4">
+    <label for="remember_me" class="inline-flex items-center">
+        <input id="remember_me" type="checkbox"
+            class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            name="remember">
+        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+    </label>
+</div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+<div class="flex items-center justify-end mt-4">
+    @if (Route::has('password.request'))
+    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+        {{ __('Forgot your password?') }}
+    </a>
+    @endif
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+    <x-button class="ml-3">
+        {{ __('Log in') }}
+    </x-button>
+</div>
+</form>
+</x-auth-card>
+</x-guest-layout> --}}
