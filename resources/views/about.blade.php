a53248d6599d99f0693c4df8db964ba8d84c8aee
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-05-12 10:59:12 +0700

    visi misi and about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,21 @@
 <x-layout>
     <x-slot name="title">
-        Tentang
+        Tentang Kami
     </x-slot>
 
     <x-slot name="subtitle">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt et vel unde, quae culpa alias repellendus.
+        PT Paragon Medika Pharma
     </x-slot>
 
     <div class="card">
         <div class="card-body">
-            Halaman ini sedang dalam tahap pengembangan.
+            <p>
+                PT Paragon Medika Pharma yang merupakan sebuah perusahaan distributor obat atau biasa disebut sebagai PBF
+                (Pedagang Besar Farmasi) yang sudah mendapatkan ijin secara hukum dalam pengadaan, penyimpanan, penyaluran,
+                dan perbekalan farmasi dalam jumlah besar sesuai undang-undang yang berlaku.
+                PT Paragon Medika Pharma sendiri terletak di Purwokerto, Jawa Tengah. PT Paragon Medika Pharma sendiri
+                melakukan distributor obat nya kepada toko-toko farmasi yang ada.
+            </p>
         </div>
     </div>
 </x-layout>
--- resources/views/visi-misi.blade.php
+++ resources/views/visi-misi.blade.php
@@ -4,12 +4,22 @@
     </x-slot>
 
     <x-slot name="subtitle">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt et vel unde, quae culpa alias repellendus.
+        Visi dan Misi PT Paragon Medika Pharma.
     </x-slot>
 
     <div class="card">
         <div class="card-body">
-            Halaman ini sedang dalam tahap pengembangan.
+            <h4>Visi</h4>
+            <p>
+                Menjadi perusahaan distribusi dan logistik yang terintegrasi di dalam bidang kesehatan, mengutamakan
+                pelayanan yang terbaik, serta dapat dipercaya dengan didukung Sumber Daya Manusia (SDM) yang berkompeten
+                serta ahli, dan didukung oleh sistem terbaik.
+            </p>
+            <h4>Misi</h4>
+            <p>
+                Turut serta membangun bangsa melalui kinerja di dalam pelayanan kesehatan, dimana dapat selalu
+                memastikan kesediaan logistik.
+            </p>
         </div>
     </div>
 </x-layout>
